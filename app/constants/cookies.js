51852d1514ff5df38dc1c8773333d54462aa71a3
John Watson <john.watson1@defra.gov.uk>
2024-03-04 11:38:32 +0000

    Rename cookie

--- app/constants/cookies.js
+++ app/constants/cookies.js
@@ -1,4 +1,4 @@
 module.exports = {
-  AUTH_COOKIE_NAME: 'ffc_ffd_auth_token',
-  SESSION_COOKIE_NAME: 'ffc_ffd_session'
+  AUTH_COOKIE_NAME: 'ffc_sfd_auth_token',
+  SESSION_COOKIE_NAME: 'ffc_sfd_session'
 }
