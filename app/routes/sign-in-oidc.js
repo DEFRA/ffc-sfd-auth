27bdb57bbf7abc7aaa06e00ccba9456491ed9eff
John Watson <42574977+johnwatson484@users.noreply.github.com>
2024-07-09 15:29:28 +0100

    Handle bookmark Defra ID (#11)

--- app/routes/sign-in-oidc.js
+++ app/routes/sign-in-oidc.js
@@ -6,19 +6,18 @@ const { GET } = require('../constants/http-verbs')
 const { validateState, decodeState, validateInitialisationVector, getAccessToken, getRedirectPath, parseJwt } = require('../auth')
 const { clearSession, setSession } = require('../session')
 
-module.exports = {
+module.exports = [{
   method: GET,
   path: '/sign-in-oidc',
   options: {
-    auth: false,
     validate: {
       query: Joi.object({
         code: Joi.string().required(),
         state: Joi.string().required()
       }).options({ stripUnknown: true }),
       failAction (request, h, err) {
-        console.log(`Defra ID login failed: ${err}`)
-        return h.view('500').takeover()
+        console.log(`Login failed: ${err}`)
+        return h.redirect('/auth/sign-in-oidc/invalid').takeover()
       }
     }
   },
@@ -27,19 +26,35 @@ module.exports = {
       return h.redirect(getRedirectPath())
     }
 
-    validateState(request, request.query.state)
-    const state = decodeState(request.query.state)
     const { access_token: accessToken, refresh_token: refreshToken } = await getAccessToken(request.query.code)
-    validateInitialisationVector(request, accessToken)
+    const organisationId = parseJwt(accessToken).currentRelationshipId
+
+    try {
+      validateState(request, request.query.state)
+      validateInitialisationVector(request, accessToken)
+    } catch (error) {
+      console.log(`Login failed: ${error}`)
+      return h.redirect(`/auth/sign-in-oidc/invalid?organisationId=${organisationId}`)
+    }
+
+    const state = decodeState(request.query.state)
     const redirect = getRedirectPath(state.redirect)
     clearSession(request, INITIALISATION_VECTOR)
     clearSession(request, STATE)
     setSession(request, REFRESH_TOKEN, refreshToken)
     setSession(request, IS_VALID, true)
 
-    const organisationId = parseJwt(accessToken).currentRelationshipId
-
     return h.redirect(`/auth/picker/defra-id?redirect=${redirect}&organisationId=${organisationId}`)
       .state(AUTH_COOKIE_NAME, accessToken, authConfig.cookieOptions)
   }
-}
+}, {
+  method: GET,
+  path: '/sign-in-oidc/invalid',
+  handler: (request, h) => {
+    clearSession(request, INITIALISATION_VECTOR)
+    clearSession(request, STATE)
+    clearSession(request, REFRESH_TOKEN)
+    clearSession(request, IS_VALID)
+    return h.view('sign-in-invalid', { organisationId: request.query.organisationId })
+  }
+}]
